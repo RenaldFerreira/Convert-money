6a3d6917db3ff7aacbb55a6602b33ae22356a3f8
RenaldFerreira <renaldferreira99@gmail.com>
2023-08-03 18:02:58 -0300

    adicionado novas moedas js

--- scripts.js
+++ scripts.js
@@ -9,6 +9,9 @@ function convertValues() {
 
     const dolarToday = 4.8
     const euroToday = 5.26
+    const libraToday = 6.21
+    const bitcoinToday = 143.005
+
 
     if (currencySelect.value == "dolar") {
 
@@ -27,6 +30,22 @@ function convertValues() {
         }).format(inputCurrencyValue / euroToday)
 
     }
+    if (currencySelect.value == "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday)
+    }
+    if (currencySelect.value == "bitcoin") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC"
+
+        }).format(inputCurrencyValue / bitcoinToday)
+
+    }
+
+
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
@@ -41,13 +60,24 @@ function changeCurrency() {
     if (currencySelect.value == "dolar") {
         currencyName.innerHTML = "Dólar Americano"
         currencyImage.src = "./assets/dolar.png"
-        
-        
-}
+
+
+    }
     if (currencySelect.value == "euro") {
         currencyName.innerHTML = "Euro"
         currencyImage.src = "./assets/euro.png"
     }
+
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "./assets/libra.png"
+    }
+
+    if (currencySelect.valor == "bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+       currencyImage.src = "./assets/bitcoin.png"
+    }
+
 
     convertValues()
 
